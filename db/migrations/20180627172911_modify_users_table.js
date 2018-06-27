2bcc510b1d3a8e9f703e6da8d35bd5652760bdf5
charleswang234 <wang.charles234@gmail.com>
2018-06-27 17:50:02 +0000

    added migrations

---
+++ db/migrations/20180627172911_modify_users_table.js
@@ -0,0 +1,24 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('users', function (table) {
+    table.string('first-name');
+    table.string('last-name');
+    table.string('email');
+    table.string('password');
+    table.dropColumn('name');
+  })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('milestones', function(table) {
+      table.dropColumn('first-name');
+      table.dropColumn('last-name');
+      table.dropColumn('email');
+      table.dropColumn('password');
+      table.string('name');
+    })
+    ]);
+};
