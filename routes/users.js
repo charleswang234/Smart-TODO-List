aae1e336323f65e2fc79f7f82212d6518c86c484
charleswang234 <wang.charles234@gmail.com>
2018-06-27 21:37:25 +0000

    added post routes for /home/:id and /logoutand get route for /home/:id/edit

--- routes/users.js
+++ routes/users.js
@@ -16,18 +16,23 @@ module.exports = (knex) => {
     // });
   });
 
+   // sends you to the edit page
+   router.get("/home/:id/edit", (req, res) => {
+      res.render("edit");
+   });
+
   // home page that allows logged in users to add and remove their items on the to do list
   router.get("/home/:id", (req, res) => {
     res.render("index");
   });
 
-  // router.post("/home/:id", (req, res) => {
-  //   if (!req.body.text) {
-  //     res.status(400).json({ error: 'invalid request: no data in POST body'});
-  //     return;
-  //   }
-
-  // });
+  router.post("/home/:id", (req, res) => {
+    if (!req.body.task_entry) {
+      res.status(400).json({ error: 'invalid request: no data in POST body'});
+      return;
+    }
+    res.redirect('/home/:id');  // change
+  });
 
 
   router.get("/", (req, res) => {
@@ -43,6 +48,11 @@ module.exports = (knex) => {
   // registration page
   router.get("/register", (req, res) => {
     res.render("register");
+  });
+
+  // user clicks the logout submit
+  router.post("/logout", (req, res) => {
+    res.redirect("/login");
   });
 
   return router;
