a02004790e4f845c171d45fcd38377b9d1736c70
charleswang234 <wang.charles234@gmail.com>
2018-06-27 20:45:08 +0000

    modified get /home and added  / routes

--- routes/users.js
+++ routes/users.js
@@ -5,15 +5,18 @@ const router  = express.Router();
 
 module.exports = (knex) => {
 
-  router.get("/home/", (req, res) => {
-    knex
-      .select("*")
-      .from("users")
-      .then((results) => {
-        res.json(results);
-    });
+  router.get("/home", (req, res) => {
+    res.render("index");                    // modify later to redirect to /home/:id if logged in and
+                                            //  redirects to login page  if not logged in
+    // knex
+    //   .select("*")
+    //   .from("users")
+    //   .then((results) => {
+    //     res.json(results);
+    // });
   });
 
+  // home page that allows logged in users to add and remove their items on the to do list
   router.get("/home/:id", (req, res) => {
     res.render("index");
   });
@@ -26,13 +29,21 @@ module.exports = (knex) => {
 
   // });
 
+
+  router.get("/", (req, res) => {
+    res.render("login");            // modify later to render a the home page if logged in
+  });
+
+
+  // login page
   router.get("/login", (req, res) => {
     res.render("login");
   });
 
+  // registration page
   router.get("/register", (req, res) => {
     res.render("register");
   });
-  
+
   return router;
 }
