c54dc44e3e957e2f8de8eecb249105ab3c85029f
thoumc <thoumc@hotmail.com>
2018-06-28 16:17:20 +0000

    cookie bug fixed on registration and bcrypt storing

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const router  = express.Router();
+const bcrypt  = require('bcryptjs')
 
 module.exports = (knex) => {
 
@@ -86,43 +87,47 @@ router.post("/home", (req, res) => {
     res.render("register");
   });
 
+  // hashing function
+  const hashing = (password) =>{
+    return bcrypt.hashSync(password, 10)
+  }
+
+
   // submitting a registration
   router.post("/register", (req, res) => {
     knex('users')
     .select('*')
     .where({ email: req.body.email })
     .then( function(result){
-      console.log(result)
 
       if (result.length === 0 ){
+
         knex('users')
         .insert({
           first_name: req.body.first_name,
           last_name: req.body.last_name,
-          password: req.body.password,
+          password: hashing(req.body.password),
           email: req.body.email
         })
-        .then(function()
+        .returning('id')
+        .then(function(id)
         {
-          console.log('inserted')
+          req.session.user_id = id[0];
+          res.redirect("/home");
+          console.log(id[0]);
+
         })
         .catch(function(error) {
           console.log(error);
         })
-        req.session.user_id = result[0].id; // THIS IS NOT WORKING
-        res.redirect("/home");
+
         return;
-        //create cookie session
-
       }
-
         //error email already exists
         res.status(400).json({ error: 'Invalid request: email already exists.'})
         console.log("error! email exists")
         return;
       })
-
-
   });
 
   // user clicks the logout submit
