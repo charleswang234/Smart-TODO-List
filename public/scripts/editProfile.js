5c8ca7e56f10c736a99865843eb45579aaef4bfd
charleswang234 <wang.charles234@gmail.com>
2018-06-29 14:03:14 +0000

    added editProfile.js and added forms to edit.ejs

---
+++ public/scripts/editProfile.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {
+  // createList();
+  // $('#addButton').on('submit', function (e){
+  //   event.preventDefault();
+  //   if (text === '') {
+  //     $('.noInputChar').fadeIn(300).delay(500).fadeOut();
+  //     return;
+  //  }else {
+
+})
